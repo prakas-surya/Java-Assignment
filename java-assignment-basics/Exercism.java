e7ab7b2d5668231f2744bad4d8fac6a058bf7c92
prakas-surya <100908266+prakas-surya@users.noreply.github.com>
2024-09-24 18:30:26 +0530

    Create Exercism.java

---
+++ java-assignment-basics/Exercism.java
@@ -0,0 +1,12 @@
+
+public class Exercism {
+
+	public static void main(String[] args) {
+		Exercism.greetings();
+	}
+	
+	public static void greetings() {
+		System.out.println("Hello World!");
+	}
+
+}
